0ffa38f82f3169427f35b6f6b33dd3e9dda91c6b
Marcantonio <marcantonio.lupercino@gmail.com>
2025-11-11 20:29:42 -0300

    creat

--- src/controller/FaculdadeController.ts
+++ src/controller/FaculdadeController.ts
@@ -1,12 +1,19 @@
+import type { promises } from "dns";
 import {AppDataSource} from "../database.js";
 import {Turma} from "../entities/Turma.js";
 
 const repoTurma = AppDataSource.getRepository(Turma);
 
 export class FaculdadeController {
-    async criarTurma(nome:string, semestre:string, id_disciplina:number){
+    async criarTurma(nome:string, semestre:string, id_disciplina:number): Promise<Turma> {
         const turma = repoTurma.create({nome,semestre,id_disciplina});
         return await repoTurma.save(turma);
     }
-};
 
+
+     async listarTurmas(): Promise<Turma[]> {
+        const sql = "SELECT * FROM turma";
+        return await repoTurma.query(sql);
+    }
+
+};
--- src/routes/FaculdadeRoutes.ts
+++ src/routes/FaculdadeRoutes.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import  {FaculdadeController}  from "../controller/FaculdadeController.js";
+import {Turma} from "../entities/Turma.js";
 
 const router = Router ();
 const faculdadeController = new FaculdadeController();
 
-router.post("/tuma",(req, res)=> {
+router.post("/tuma",async (req, res)=> {
     const {nome,semestre,id_disciplina} = req.body;
-    const turma = faculdadeController.criarTurma(nome,semestre,id_disciplina);
+    const turma = await faculdadeController.criarTurma(nome,semestre,id_disciplina);
+    res.json(turma);
+});
+
+router.get("/tuma",async (req, res)=> {
+    const turma = await faculdadeController.listarTurmas();
     res.json(turma);
 });
 
